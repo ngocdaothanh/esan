f3429ab7cbe1fc497ca57a684f5296b2db02f108
Ngoc Dao <ngocdaothanh@gmail.com>
2009-06-21 17:34:45 +0900

    valid -> acceptable

--- src/esan.erl
+++ src/esan.erl
@@ -6,75 +6,82 @@
 
 -import(xmerl_xs, [xslapply/2]).
 
-default_valid_tags() ->
-	[a, b, big, br, center, dd, dl, dt, em, embed, h1, h2, h3, h4, h5, h6, i, img, li, menu, object, ol, p, small, strike, strong, table, td, th, tr, tt, u, ul].
+default_acceptable_tags() ->
+    [a, abbr, acronym, address, area, b, big, blockquote, br, caption, center,
+    cite, code, col, colgroup, dd, del, dfn, dir, 'div', dl, dt, em, embed,
+    font, h1, h2, h3, h4, h5, h6, hr, i, img, ins, kbd, label, legend, li,
+    menu, object, ol, p, pre, q, s, samp, small, span, strike, strong, sub, sup,
+    table, tbody, td, tfoot, th, thead, tr, tt, u, ul].
 
-default_valid_attributes() ->
-	[align, allowfullscreen, alt, height, href, hspace, src, type, vspace, width].
-
+default_acceptable_attributes() ->
+    [abbr, align, allowfullscreen, alt, axis, border, cellpadding, cellspacing,
+    cols, colspan, dir, height, href, hreflang, hspace, label, nohref, noshade,
+    nowrap, prompt, rel, rev, rows, rowspan, rules, size, span, src, title,
+    type, valign, vspace, width].
+    
 sanitize(Html) ->
-	sanitize(Html, default_valid_tags(), default_valid_attributes()).
+    sanitize(Html, default_acceptable_tags(), default_acceptable_attributes()).
 
-sanitize(Html, ValidTags, ValidAttributes) ->
-	try
-		put(valid_tags, ValidTags),
-		put(valid_attributes, ValidAttributes),
+sanitize(Html, AcceptableTags, AcceptableAttributes) ->
+    try
+        put(acceptable_tags, AcceptableTags),
+        put(acceptable_attributes, AcceptableAttributes),
 
-		% Html may not be inside a single element, thus we put it inside one
-		WithRoot = "<root>" ++ Html ++ "</root>",
+        % Html may not be inside a single element, thus we put it inside one
+        WithRoot = "<root>" ++ Html ++ "</root>",
 
-		{E, _Rest} = xmerl_scan:string(WithRoot),
-		Result = xslapply(fun sanitize_xml/1, E),
-		{ok, Result}
-	catch
-		_ : Reason -> {error, Reason}
-	end.
+        {E, _Rest} = xmerl_scan:string(WithRoot),
+        Result = xslapply(fun sanitize_xml/1, E),
+        {ok, Result}
+    catch
+        _ : Reason -> {error, Reason}
+    end.
 
 sanitize_xml(E = #xmlElement{name = root}) ->
-	xslapply(fun sanitize_xml/1, E);
+    xslapply(fun sanitize_xml/1, E);
 
 sanitize_xml(E = #xmlElement{}) ->
-	ValidTags = get(valid_tags),
-	Name = E#xmlElement.name,
-	case lists:member(Name, ValidTags) of
-		true ->
-			case E#xmlElement.content of
-				[] ->
-					case E#xmlElement.attributes of
-						[]         -> ["<", atom_to_list(Name),                                       " />"];
-						Attributes -> ["<", atom_to_list(Name), " ", sanitize_attributes(Attributes), " />"]
-					end;
+    AcceptableTags = get(acceptable_tags),
+    Name = E#xmlElement.name,
+    case lists:member(Name, AcceptableTags) of
+        true ->
+            case E#xmlElement.content of
+                [] ->
+                    case E#xmlElement.attributes of
+                        []         -> ["<", atom_to_list(Name),                                       " />"];
+                        Attributes -> ["<", atom_to_list(Name), " ", sanitize_attributes(Attributes), " />"]
+                    end;
 
-				_ ->
-					OpenTag = case E#xmlElement.attributes of
-						[]         -> ["<", atom_to_list(Name),                                       ">"];
-						Attributes -> ["<", atom_to_list(Name), " ", sanitize_attributes(Attributes), ">"]
-					end,
-					CloseTag = ["</", atom_to_list(Name), ">"],
-					[OpenTag, xslapply(fun sanitize_xml/1, E), CloseTag]
-			end;
+                _ ->
+                    OpenTag = case E#xmlElement.attributes of
+                        []         -> ["<", atom_to_list(Name),                                       ">"];
+                        Attributes -> ["<", atom_to_list(Name), " ", sanitize_attributes(Attributes), ">"]
+                    end,
+                    CloseTag = ["</", atom_to_list(Name), ">"],
+                    [OpenTag, xslapply(fun sanitize_xml/1, E), CloseTag]
+            end;
 
-		false -> ""
-	end;
+        false -> ""
+    end;
 
 sanitize_xml(T = #xmlText{}) ->
-	T#xmlText.value.
+    T#xmlText.value.
 
 sanitize_attributes(Attributes) ->
-	ValidAttributes = get(valid_attributes),
-	Strings = lists:foldr(
-		fun(A, Acc) ->
-			Name = A#xmlAttribute.name,
-			case lists:member(Name, ValidAttributes) of
-				true ->
-					Value = A#xmlAttribute.value,
-					[atom_to_list(Name) ++ "=\"" ++ Value ++ "\"" | Acc];
+    AcceptableAttributes = get(acceptable_attributes),
+    Strings = lists:foldr(
+        fun(A, Acc) ->
+            Name = A#xmlAttribute.name,
+            case lists:member(Name, AcceptableAttributes) of
+                true ->
+                    Value = A#xmlAttribute.value,
+                    [atom_to_list(Name) ++ "=\"" ++ Value ++ "\"" | Acc];
 
-				false ->
-					Acc
-			end
-		end,
-		[],
-		Attributes
-	),
-	string:join(Strings, " ").
+                false ->
+                    Acc
+            end
+        end,
+        [],
+        Attributes
+    ),
+    string:join(Strings, " ").
--- test/esan_tests.erl
+++ test/esan_tests.erl
@@ -3,6 +3,6 @@
 -compile(export_all).
 
 test() ->
-	{ok, Html1} = file:read_file("test/test.html"),
-	{ok, Html2} = esan:sanitize(binary_to_list(Html1)),
-	io:format("~p~n", [Html2]).
+    {ok, Html1} = file:read_file("test/test.html"),
+    {ok, Html2} = esan:sanitize(binary_to_list(Html1)),
+    io:format("~p~n", [Html2]).
